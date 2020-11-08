b0576955baedd9c431539b2e00bb40c2516e445f
aidant19 <aidant19@gmail.com>
2020-11-08 02:14:05 -0500

    Beginning animation testing

--- src/main/java/cc/GraphPanel.java
+++ src/main/java/cc/GraphPanel.java
@@ -4,26 +4,35 @@ import java.awt.*;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import java.lang.Math;
+import java.util.ArrayList;
 
 public class GraphPanel extends JPanel {
 
     Vertex[] verts;
+    ArrayList<Vertex> infectedVerts;
     int[][] edgeArray;
     int maxRadius;
     int dimension;
+    int size;
 
     public GraphPanel(int nVerts) {
         super();
         dimension = 800;
         setPreferredSize(new Dimension(dimension, dimension));
         setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
-        int size = 10;
+        size = 10;
         maxRadius = 300;
         edgeArray = GraphGenerator.generate(size);
         verts = new Vertex[size];
         for (int i = 0; i < verts.length; i++) {
             verts[i] = new Vertex(maxRadius);
         }
+        for (int[] edge : edgeArray) {
+            verts[edge[0]].connections.add(edge[1]);
+        }
+        infectedVerts = new ArrayList<>();
+        update(true);
     }
 
     public void paint(Graphics g) {
@@ -34,5 +43,23 @@ public class GraphPanel extends JPanel {
             g.drawLine(verts[edge[0]].x, verts[edge[0]].y, verts[edge[1]].x, verts[edge[1]].y);
         }
         g.drawOval((dimension / 2) - maxRadius, (dimension / 2) - maxRadius, 2 * maxRadius, 2 * maxRadius);
+    }
+
+    public void update(boolean init) {
+        if (init) {
+            int index = (int) (Math.random() * size);
+            verts[index].infected = true;
+            infectedVerts.add(verts[index]);
+        } else {
+            for (Vertex infVert : infectedVerts) {
+                if (Math.random() > 0.75) {
+                    Vertex exposed = verts[infVert.connections.get((int) Math.random() * infVert.connections.size())];
+                    if (!infectedVerts.contains(exposed)) {
+                        exposed.infected = true;
+                        infectedVerts.add(exposed);
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/cc/Main.java
+++ src/main/java/cc/Main.java
@@ -1,8 +1,10 @@
 package cc;
 
+import java.lang.Thread;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         GraphPanel gPanel = new GraphPanel(10);
         AppWindow win = new AppWindow();
         win.add(gPanel);
